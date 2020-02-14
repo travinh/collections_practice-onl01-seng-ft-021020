5ecf42d51bf06fb8aeacb78d256675266f708c6a
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-14 09:17:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -65,7 +65,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do {|element, index|}
+  array.each_with_index.collect {|element, index|}
     if index!=1 
       element << "s"
     end
